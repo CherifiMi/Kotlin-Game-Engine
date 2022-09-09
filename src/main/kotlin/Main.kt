98f2adad2eed1314d4fd76369b9c9e85b7300507
CherifiMi <cherifi154949@gmail.com>
2022-09-09 20:01:06 +0100

    window

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,6 @@
+import engine.Window
+
+fun main(){
+    val window: Window = Window.get()
+    window.run()
+}
---
+++ src/main/kotlin/engine/Window.kt
@@ -0,0 +1,74 @@
+package engine
+
+import org.lwjgl.Version
+import org.lwjgl.glfw.GLFW.*
+import org.lwjgl.glfw.GLFWErrorCallback
+import org.lwjgl.opengl.GL
+import org.lwjgl.opengl.GL11.*
+import org.lwjgl.system.MemoryUtil.NULL
+
+class Window() {
+    private val width = 1920/2
+    private val height = 1080/2
+    private val title = "KGE"
+    private var glfwWindow: Long = 0L
+
+    companion object {
+        private var window: Window? = null
+
+        fun get(): Window {
+            if (window == null) {
+                window = Window()
+            }
+            return window!!
+        }
+    }
+
+    fun run(){
+        println("hillo LWJGL " + Version.getVersion())
+        init()
+        loop()
+    }
+
+    private fun init() {
+        // setup error call back
+        GLFWErrorCallback.createPrint(System.err).set()
+
+        // init glfw
+        if (!glfwInit()){
+            throw(java.lang.IllegalStateException("Unable to init GLFW."))
+        }
+
+        // config glfw
+        glfwDefaultWindowHints()
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE)
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE)
+        glfwWindowHint(GLFW_MAXIMIZED, GLFW_FALSE)
+
+        //  create window
+        glfwWindow = glfwCreateWindow(this.width, this.height, this.title, NULL, NULL)
+        if (glfwWindow == NULL){
+            throw(IllegalStateException("Fail to create GLFW window."))
+        }
+
+        // make OpenGL context current
+        glfwMakeContextCurrent(glfwWindow)
+        //Enable v-sync
+        glfwSwapInterval(1)
+
+        // make window visible
+        glfwShowWindow(glfwWindow)
+
+        GL.createCapabilities()
+    }
+    private fun loop() {
+        while (!glfwWindowShouldClose(glfwWindow)){
+            // poll events
+            glfwPollEvents()
+
+            glClearColor(0.180f, 0.95f, 0.61f, 1f)
+            glClear(GL_COLOR_BUFFER_BIT)
+            glfwSwapBuffers(glfwWindow)
+        }
+    }
+}
