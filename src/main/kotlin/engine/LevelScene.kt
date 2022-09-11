ededd2666ce253a6223820d9d85820137f565835
CherifiMi <cherifi154949@gmail.com>
2022-09-11 10:20:06 +0100

    clean testing stuff init openGL

--- src/main/kotlin/engine/LevelScene.kt
+++ src/main/kotlin/engine/LevelScene.kt
@@ -1,14 +1,6 @@
 package engine
 
 class LevelScene : Scene() {
-    private val window = Window.get()
-
-    init {
-        println("Inside level scene")
-        window.r = 1f
-        window.g = 1f
-        window.b = 1f
-    }
 
     override fun update(dt: Float) {}
 }
--- src/main/kotlin/engine/Scene.kt
+++ src/main/kotlin/engine/Scene.kt
@@ -1,9 +1,6 @@
 package engine
 
 abstract class Scene {
-    init {
-        println("Insid level scene")
-    }
     abstract fun update(dt: Float)
 
 }
