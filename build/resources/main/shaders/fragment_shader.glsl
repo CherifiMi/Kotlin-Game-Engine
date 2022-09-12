79527559bb06282166f073d9f75a6a1e696c94d5
CherifiMi <cherifi154949@gmail.com>
2022-09-13 00:41:06 +0100

    clean up

--- build/resources/main/shaders/fragment_shader.glsl
+++ build/resources/main/shaders/fragment_shader.glsl
@@ -11,7 +11,5 @@ out vec4 color;
 
 void main()
 {
-    //vec2 uv = gl_FragCoord.xy/sin(uTime);
-    //color = vec4(0.5 + 0.5*sin(uTime+uv.yxy),1.0) / fColor;
     color = texture(TEX, fTexCoords);
 }
